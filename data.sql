10405a32cf8f0ff0f4a71160673adb4098b1b79a
Andrea Arias <ariasdrea@gmail.com>
2019-08-19 11:41:13 +0200

    removed comments

--- data.sql
+++ data.sql
@@ -1,9 +1,7 @@
--- if a change is made to the table, dropping the table will delete the old table and replace it with the new table
 DROP TABLE IF EXISTS signatures;
 DROP TABLE IF EXISTS user_profiles;
 DROP TABLE IF EXISTS users;
 
--- For Registration
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     first VARCHAR(200) NOT NULL,
@@ -17,7 +15,6 @@ CREATE TABLE users(
 CREATE TABLE signatures(
     id SERIAL PRIMARY KEY,
     signature TEXT NOT NULL,
-    -- this links the signature to the correct user via id
     user_id INTEGER UNIQUE NOT NULL REFERENCES users(id),
     ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -27,6 +24,5 @@ CREATE TABLE user_profiles(
     age INTEGER,
     city VARCHAR(200),
     url VARCHAR(300),
-    -- this links the age, city, etc to the correct user via id
     user_id INTEGER UNIQUE NOT NULL REFERENCES users(id)
 );
