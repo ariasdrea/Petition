f76cc7b00b52ffca24547965a1b76c9f6110e781
Andrea Arias <ariasdrea@gmail.com>
2019-08-23 11:35:17 +0200

    created tests to check if middleware is working - passing

--- index.test.js
+++ index.test.js
@@ -7,66 +7,54 @@ const { app }  = require("./index");
 // requiring  cookie-session that lives in the '__mocks__ directory' - jest knows to look in there
 const cookieSession = require("cookie-session");
 
-test.only('logged out users get redirected to register when they try to go to petition', () => {
+test('logged out users get redirected to register when they try to go to petition', () => {
+    cookieSession.mockSessionOnce({
+        userId: null
+    });
     return supertest(app).get("/petition").then(res => {
         expect(res.statusCode).toBe(302);
-        expect(res.headers.location).toBe('/registration');
+        expect(res.headers.location).toBe('/register');
     });
 });
 
-
-
-
-test("GET /home returns as an h1 as response", () => {
-    //supertest takes in the server as an argument
-    return supertest(app)
-        .get("/home")
-        .then(res => {
-            expect(res.statusCode).toBe(200);
-            expect(res.text).toBe("<h1>home</h1>");
-            expect(res.headers["content-type"]).toContain("text/html");
-        });
-});
-
-test("GET /home with no cookies causes me to be redirected", () => {
-    return supertest(app)
-        .get("/home")
-        .then(res => {
-            // console.log("location header:", res.headers.location);
-            //I want to check if I'm actually being redirected (status code 302)
-            expect(res.statusCode).toBe(302);
-            expect(res.headers.location).toBe("/registration");
-        });
-});
-
-//test if we get the correct response when we do have a cookie.
-test('GET /home request sends H1 as response when "whatever" cookie is sent!', () => {
+test("logged in users get redirected to petition when they go to registration", () => {
     cookieSession.mockSessionOnce({
-        whatever: true
+        userId: 1
     });
-    return supertest(app)
-        .get("/home")
-        .then(res => {
-            expect(res.statusCode).toBe(200);
-        });
+    return supertest(app).get('/register').then(res => {
+        expect(res.statusCode).toBe(302);
+        expect(res.headers.location).toBe('/petition');
+    });
 });
 
-//now when we use supertest to make our request to the server, the fake cookie will automatically be sent along with the request (w/o us explicitely telling it to do so)
-
-test('POST /welcome should set "submitted" cookie', () => {
-    // send over an empty cookie as part of the request I make so that the server has an aempty cookie to put data into.
-    //in this case, data refers to {submitted: true}
-    let cookie = {};
-    cookieSession.mockSessionOnce(cookie);
-    //next step is to use supertest to make a POST request
-    return supertest(app)
-        .post("/welcome")
-        .then(() => {
-            //the cookie will be in the object variable (obj) we created up above
-            console.log("cookie variable: ", cookie);
-        });
+test("logged in users get redirected to petition when they go to login", () => {
+    cookieSession.mockSessionOnce({
+        userId: 1,
+    });
+    return supertest(app).get('/login').then(res => {
+        expect(res.statusCode).toBe(302);
+        expect(res.headers.location).toBe('/petition');
+    });
 });
-// Me seal with MockSessionOnce in 2 cases.
-//we need to receive a cookie as part of the response
-//we will need to work with mockSessionOnce if we want to 1. send a test / dummy cookie as part of the request we make to the server AND
-//2. if we want to see the cookie we receive as part of the response. We need to check that a cookie has been set.
+
+test("logged in users who haven't signed get redirected to petition when they try to go to thank you page", () => {
+    cookieSession.mockSessionOnce({
+        userId: 1,
+        sigId: null
+    });
+    return supertest(app).get('/thanks').then(res => {
+        expect(res.statusCode).toBe(302);
+        expect(res.headers.location).toBe('/petition');
+    });
+});
+
+test("logged in users who haven't signed get redirected to petition when they try to go to signers page", () => {
+    cookieSession.mockSessionOnce({
+        userId: 1,
+        sigId: null
+    });
+    return supertest(app).get('/signers').then(res => {
+        expect(res.statusCode).toBe(302);
+        expect(res.headers.location).toBe('/petition');
+    });
+});
